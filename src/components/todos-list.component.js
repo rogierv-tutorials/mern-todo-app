1710930859a5e69a2d3ccf49ae77e1c04935e099
Rogier Verkaik <rogierv@gmail.com>
2018-12-18 07:29:59 +0100

    Added router and components

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+  render () {
+    return (
+      <div>
+        <p>Welcome to Todos List Components!!</p>
+      </div>
+    )
+  }
+}
